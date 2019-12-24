8f738c1059e48fc68fc1b7da2b29ef4a3af4abf3
pffStoner <acid901@mail.bg>
2019-12-24 14:50:33 +0200

    adding build

---
+++ build/precache-manifest.d1a0088c3df5e3e9d88da138c66d1f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0777906880c39d795c1423dc198555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a18848be663425a7e49d",
+    "url": "/static/css/main.bfc67fc4.chunk.css"
+  },
+  {
+    "revision": "6d94015c33973ffa9146",
+    "url": "/static/js/2.c186727b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c186727b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a18848be663425a7e49d",
+    "url": "/static/js/main.627635b8.chunk.js"
+  },
+  {
+    "revision": "c644b75275d91bee4b40",
+    "url": "/static/js/runtime-main.63480e1f.js"
+  }
+]);
